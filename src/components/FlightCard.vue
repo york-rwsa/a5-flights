4a2b11af52c02a1234a632c3043591981da54092
Rory Anderson <rory@jragon.co.uk>
2018-10-31 16:03:18 +0000

    add basic flight card

---
+++ src/components/FlightCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <b-card>
+    <b-row>
+      <b-col>
+        <b-row class="align-items-center">
+          <b-col cols="3">
+            <span class="align-middle"><b>OUTBOUND</b></span>
+          </b-col>
+          <b-col>
+            <b-row>
+              <b-col><p><b>08:15 - 17:00</b></p></b-col>
+              <b-col><p><b>2 stops</b></p></b-col>
+              <b-col><p><b>10 Hours</b></p></b-col>
+            </b-row>
+            <b-row>
+              <b-col><p>United Airlines</p></b-col>
+              <b-col><p>LGW, LHR</p></b-col>
+              <b-col><p>MAN - LAX</p></b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+        <hr/>
+        <b-row class="align-items-center">
+          <b-col cols="3">
+            <span class="align-middle"><b>RETURN</b></span>
+          </b-col>
+          <b-col>
+            <b-row>
+              <b-col><p><b>10:25 - 19:00</b></p></b-col>
+              <b-col><p><b>1 stop</b></p></b-col>
+              <b-col><p><b>8 Hours</b></p></b-col>
+            </b-row>
+            <b-row>
+              <b-col><p>United Airlines</p></b-col>
+              <b-col><p>LGW</p></b-col>
+              <b-col><p>LAX - MAN</p></b-col>
+            </b-row>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col cols="2" class="d-flex align-items-end flex-column">
+        <h4 class="mb-auto">£20</h4>
+        <b-button class="">Book Now!</b-button>
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<style>
+.pull-bottom {
+    display: inline-block;
+    vertical-align: bottom;
+    float: none;
+}
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-row class="my-3">
+    <b-col cols="3"></b-col>
+    <b-col>
+      <flight-card class="my-4" v-for="i in 5" :key="i"></flight-card>
+    </b-col>
+  </b-row>
 </template>
+
+<script>
+import FlightCard from '@/components/FlightCard'
+
+export default {
+  components: {
+    FlightCard
+  }
+}
+</script>
