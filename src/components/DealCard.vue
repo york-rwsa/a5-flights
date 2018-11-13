830d90920d0eb06922c3d2fb97c8d33f995a18a8
Rory Anderson <rory@jragon.co.uk>
2018-11-13 13:25:34 +0000

    refactor to deal card and add some more text #11

---
+++ src/components/DealCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-card :title="deal.title"
+          :img-src="deal.img"
+          class="h-100">
+    <p class="card-text">
+      {{ deal.text }}
+    </p>
+    <div slot="footer">
+      <b-button class="float-right" to="bookflight" primary>More Detials</b-button>
+    </div>    
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: ['deal']
+}
+</script>
+
+<style>
+.card-img {
+  height: 40%;
+  background-size: cover;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,38 +14,9 @@
         </b-card>
       </b-col>
     </b-row>
-    <b-row>
-      <b-col cols="3">
-        <b-card title="£100 for a week in Edinburgh"
-                :img-src="iconURL">
-          <p class="card-text">
-            Full room and board with everything included amazing
-          </p>
-        </b-card>
-      </b-col>
-      <b-col cols="3">
-        <b-card title="£100 for a week in Edinburgh"
-                :img-src="iconURL">
-          <p class="card-text">
-            Full room and board with everything included amazing
-          </p>
-        </b-card>
-      </b-col>
-      <b-col cols="3">
-        <b-card title="£100 for a week in Edinburgh"
-                :img-src="iconURL">
-          <p class="card-text">
-            Full room and board with everything included amazing
-          </p>
-        </b-card>
-      </b-col>
-      <b-col cols="3">
-        <b-card title="£100 for a week in Edinburgh"
-                :img-src="iconURL">
-          <p class="card-text">
-            Full room and board with everything included amazing
-          </p>
-        </b-card>
+    <b-row class="mt-5">
+      <b-col v-for="(deal, index) in deals" :key="index" cols="3">
+        <deal-card :deal="deal"></deal-card>
       </b-col>
     </b-row>
   </b-container>
@@ -53,15 +24,38 @@
 
 <script>
 import FlightSearch from '@/components/FlightSearch'
+import DealCard from '@/components/DealCard'
 
 export default {
-  components: {
-    FlightSearch
-  },
-  computed: {
-    iconURL () {
-      return require('@/assets/edinburgh.jpg')
+  data () {
+    return {
+      deals: [
+        {
+          title: '£100 for a week in Edinburgh',
+          text: 'Full room and board with everything included amazing',
+          img: require('@/assets/edinburgh.jpg')
+        },
+        {
+          title: '£600 pp for a week in Majorca',
+          text: 'Discover the magical island. Try some authentic meals, strong drinks, and good parties.',
+          img: require('@/assets/majorca.jpg')
+        },
+        {
+          title: '£250 ferry to Ireland',
+          text: 'Drive around Ireland! Experience the joys of Irish weather. Gaze upon the beutiful coastline. Listen to neo-trad music.',
+          img: require('@/assets/ireland.jpg')
+        },
+        {
+          title: '£400 pp for a week in Majorca',
+          text: 'Tour the amazing beaches of Thailand. Get to know the beautiful culture.',
+          img: require('@/assets/thailand.jpg')
+        }
+      ]
     }
+  },
+  components: {
+    FlightSearch,
+    DealCard
   }
 }
 </script>
