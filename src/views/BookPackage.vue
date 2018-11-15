a2604f1169662d9b57753055eae455a10b77765f
Rory Anderson <rory@jragon.co.uk>
2018-11-15 11:19:19 +0000

    why is life so hard? :cry: commit -am doesn't add untracked files whoops #16

---
+++ src/views/BookPackage.vue
@@ -0,0 +1,19 @@
+
+<script>
+export default {
+  computed: {
+    loading () {
+      return this.$loading({
+        lock: true,
+        background: 'rgba(0, 0, 0, 0.7)'
+      })
+    }
+  },
+  mounted () {
+    this.loading.text = 'Redirecting to package booking site'
+  },
+  destroyed () {
+    this.loading.close()
+  }
+}
+</script>
