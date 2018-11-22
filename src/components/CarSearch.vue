027f675313135aa04bb081df617a9baa1cb340d4
Rory Anderson <rory@jragon.co.uk>
2018-11-22 14:06:15 +0000

    fix breakpoints on mobile #17

--- src/components/CarSearch.vue
+++ src/components/CarSearch.vue
@@ -10,46 +10,50 @@
           tabindex="1"
         ></v-autocomplete>
       </b-col>
-      <b-col cols="2" md="2">
-        <label for="pickupDate">Pickup Date</label>
-      </b-col>
-      <b-col cols="6" md="4">
-        <b-input id="pickupDate"
-                 type="date"
-                 v-model="form.pickupDate"
-                 tabindex="2"></b-input>
+      <b-col>
+        <b-row>
+          <b-col cols="12" md="4">
+            <label for="pickupDate">Pickup Date</label>
+          </b-col>
+          <b-col cols="10" md="8">
+            <b-input id="pickupDate"
+                      type="date"
+                      v-model="form.pickupDate"
+                      tabindex="2"></b-input>
+          </b-col>
+        </b-row>
+        <b-row class="mt-2">
+          <b-col cols="12" md="4">
+            <label for="dropoffDate">Dropoff Date</label>
+          </b-col>
+          <b-col  cols="10" md="8">
+            <b-input id="dropoffDate"
+                      type="date"
+                      v-model="form.dropoffDate"
+                      tabindex="3"></b-input>
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
     <b-row class="mt-3 align-items-center">
-      <b-col>
-        <b-row>
-          <b-col>
-            <b-form-checkbox checked
-                         tabindex="4"
-                         v-model="form.driverOver25">
-              Driver over the age of 25
-            </b-form-checkbox>
-          </b-col>
-        </b-row>
-        <b-row v-if="!form.driverOver25" class="mt-2 align-items-center">
-          <b-col>
+      <b-col cols="12" md="4">
+        <b-form-checkbox checked
+                      tabindex="4"
+                      v-model="form.driverOver25">
+          Driver over the age of 25
+        </b-form-checkbox>
+      </b-col>
+      <b-col v-if="!form.driverOver25" cols="12" md="4">
+        <b-row class="align-items-center">
+          <b-col md="5" cols="6">
             <label for="driverAge">Driver Age</label>
           </b-col>
-          <b-col>
+          <b-col cols="4" md="5">
             <b-form-select v-model="form.driversAge"
-                           :options="form.ageOptions"
+                            :options="form.ageOptions"
             ></b-form-select>
           </b-col>
         </b-row>
-      </b-col>
-      <b-col cols="2" md="2">
-        <label for="dropoffDate">Dropoff Date</label>
-      </b-col>
-      <b-col cols="6" md="4">
-        <b-input id="dropoffDate"
-                 type="date"
-                 v-model="form.dropoffDate"
-                 tabindex="3"></b-input>
       </b-col>
     </b-row>
     <b-row class="mt-3">
